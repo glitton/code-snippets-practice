97917a9b80e2fc85a99a20c176a9f8cb3a5fdf13
Generosa Litton <glcodeworks@gmail.com>
2024-10-31 08:31:40 -0500

    five letter wip

---
+++ reverseFiveLetters.js
@@ -0,0 +1,5 @@
+// Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+console.log(reverseFiveLetters("Hey fellow warriors")); //--> "Hey wollef sroirraw"
+console.log(reverseFiveLetters("This is a test")); //--> "This is a test"
+console.log(reverseFiveLetters("This is another test")); //--> "This is rehtona test"
